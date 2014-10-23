cc251eed3f6f9fa312627191235e142803661eb3
Denis Nelubin <dnelubin@gmail.com>
2014-10-23 11:44:14 +0700

    Added main activity with Navigation Drawer.

---
+++ app/src/main/java/com/lendamage/android/tasks/TasksActivity.java
@@ -0,0 +1,81 @@
+package com.lendamage.android.tasks;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TasksActivity extends Activity implements TasksFragment.OnFragmentInteractionListener {
+
+    public static final String EXTRA_TASK_PATH = "taskPath";
+
+    private String taskPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+
+        Intent intent = getIntent();
+        this.taskPath = intent.getStringExtra(EXTRA_TASK_PATH);
+        if (this.taskPath == null) {
+            this.taskPath = "";
+        }
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, TasksFragment.newInstance(this.taskPath))
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(String taskName) {
+        Intent intent = new Intent(getBaseContext(), TasksActivity.class);
+        intent.putExtra(EXTRA_TASK_PATH, this.taskPath + "/" + taskName);
+        startActivity(intent);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_placeholder, container, false);
+            return rootView;
+        }
+    }
+
+}
